ab5c7b8ff9dcf8eff20b3a02c7769106aa2f3a0c
Tom Clune <thomas.l.clune@nasa.gov>
2021-09-12 18:41:26 -0400

    Update scenarios/generic_algorithms/alt_sparse_array_m.F90
    Co-authored-by: Brad Richardson <everythingfunctional@protonmail.com>

--- scenarios/generic_algorithms/alt_sparse_array_m.F90
+++ scenarios/generic_algorithms/alt_sparse_array_m.F90
@@ -166,7 +166,7 @@ program example_usage
     use sparse_array_m(integer), only: int_sparse_array => sparse_array_t
     use sparse_array_m(varying_string), only: string_sparse_array => sparse_array_t
 
-    use sparse_arary_map_m(integer, varying_string: map
+    use sparse_arary_map_m(integer, varying_string), only: map
     use sparse_arary_filter_m(varying_string), only: filter
     use sparse_arary_reduce_m(varying_string, ...), only: reduce
 
