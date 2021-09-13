711e91fee6648f350d4ed854f74bd245b14fc8e5
Brad Richardson <everythingfunctional@protonmail.com>
2021-09-13 14:25:42 -0500

    add template arguments in more places

--- scenarios/Generic_Error_Return/forwarding_errors.f90
+++ scenarios/Generic_Error_Return/forwarding_errors.f90
@@ -17,8 +17,13 @@ module fallible_m
         module procedure from_error
         module procedure from_answer
     end interface
+
+    interface and_then
+        module procedure and_then_2_args
+        ...
+    end interface
 contains
-    function from_error(error) result(fallible)
+    function from_error<err, res>(error) result(fallible)
         type(err), intent(in) :: error
         type(fallible_t<err, res>) :: fallible
 
@@ -26,7 +31,7 @@ contains
         fallible%is_err_ = .true.
     end function
 
-    function from_answer(answer) result(fallible)
+    function from_answer<err, res>(answer) result(fallible)
         type(res), intent(in) :: answer
         type(fallible_t<err, res>) :: fallible
 
@@ -63,7 +68,7 @@ contains
         end if
     end function
 
-    function and_then_2_args(func, maybe_a, maybe_b) result(maybe_answer)
+    function and_then_2_args<err, x, y, res>(func, maybe_a, maybe_b) result(maybe_answer)
         abstract interface
             function func_i(a, b) result(maybe_answer)
                 type(x), intent(in) :: a
@@ -72,8 +77,8 @@ contains
             end function
         end interface
         procedure(func_i) :: func
-        type(fallible_t<err, T>), intent(in) :: maybe_a
-        type(fallible_t<err, U>), intent(in) :: maybe_b
+        type(fallible_t<err, x>), intent(in) :: maybe_a
+        type(fallible_t<err, y>), intent(in) :: maybe_b
         type(fallible_t<err, res>) :: maybe_answer
 
         if (any([maybe_a%is_err_, maybe_b%is_err_])) then
@@ -94,11 +99,12 @@ contains
 end module
 
 program example
-    use erloff, only: error_list_t
-    use fallible_m, only: fallible_t
+    use erorr_m, only: error_t
+    use fallible_m, only: fallible_t, and_then
+    use thing_m, only: thing_t
 
     associate(maybe => &
-            and_then_2_args( & ! This forwards the error along
+            and_then<error_t, integer, string_t, thing_t>( & ! This forwards the error along
                 something_else_error_prone, & ! This shouldn't get called
                 divide(42, 0), & ! This generates an error
                 repeat_with_limit("Hello", 5, 50))) ! This generates a valid answer
@@ -113,36 +119,36 @@ program example
 contains
     function divide(numerator, denomenator) result(maybe_quotient)
         integer, intent(in) :: numerator, denomenator
-        type(fallible_t<error_list_t, integer>) :: maybe_quotient
+        type(fallible_t<error_t, integer>) :: maybe_quotient
 
         if (denomenator == 0) then
-            maybe_quotient = fallible_t(error_list_t("tried to divide by zero"))
+            maybe_quotient = fallible_t<error_t, integer>(error_t("tried to divide by zero"))
         else
-            maybe_quotient = fallible_t(numerator / denomenator)
+            maybe_quotient = fallible_t<error_t, integer>(numerator / denomenator)
         end if
     end function
 
     function repeat_with_limit(string, repetitions, limit) result(maybe_string)
         type(string_t), intent(in) :: string
         integer, intent(in) :: repetitions, limit
-        type(fallible_t<error_list_t, string_t>) :: maybe_string
+        type(fallible_t<error_t, string_t>) :: maybe_string
 
         if (len(string) * repetitions > limit) then
-            maybe_string = fallible_t(error_list_t("tried to make string too long"))
+            maybe_string = fallible_t<error_t, string_t>(error_t("tried to make string too long"))
         else
-            maybe_string = fallible_t(repeat(string, repetitions))
+            maybe_string = fallible_t<error_t, string_t>(repeat(string, repetitions))
         end if
     end function
 
     function something_else_error_prone(num, string) result(maybe_thing)
         integer, intent(in) :: num
         type(string_t), intent(in) :: string
-        type(fallible_t<error_list_t, thing_t>) :: maybe_thing
+        type(fallible_t<error_t, thing_t>) :: maybe_thing
 
         if (invalid_input(num, string)) then
-            maybe_thing = fallible_t(error_list_t("invalid for some reason"))
+            maybe_thing = fallible_t<error_t, thing_t>(error_t("invalid for some reason"))
         else
-            maybe_thing = fallible_t(thing_t(...))
+            maybe_thing = fallible_t<error_t, thing_t>(thing_t(...))
         end if
     end function
 end program
