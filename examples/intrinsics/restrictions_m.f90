480f9fe943d3634370cd2f9bd904b8af7f9a9327
Brad Richardson <everythingfunctional@protonmail.com>
2022-07-05 15:51:43 -0500

    feat: initial implementation of findloc template

---
+++ examples/intrinsics/restrictions_m.f90
@@ -0,0 +1,26 @@
+module restrictions_m
+    implicit none
+    private
+    public :: equatable, comparable
+
+    restriction equatable(T, equals)
+        type :: T
+        end type
+        interface
+            function equals(x, y)
+                type(T), intent(in) :: x, y
+                logical :: equals
+            end function
+        end interface
+    end restriction
+
+    restriction comparable(T, equals, less_than)
+        requires equatable(T, equals)
+        interface
+            function less_than(x, y)
+                type(T), intent(in) :: x, y
+                logical :: less_than
+            end function
+        end interface
+    end restriction
+end module
