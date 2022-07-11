5e62c455de1d415cfb225ce9d32a0d918c4eef88
Tom Clune <thomas.l.clune@nasa.gov>
2022-07-11 14:38:21 -0400

    Extended matmul example
    - Gave more thought to the names for the restrictions.
    - Introduced variant driver that exercises template for block
      matrices.

--- examples/matmul/main.f90
+++ examples/matmul/main.f90
@@ -1,14 +1,94 @@
 program main
-    use matmul_m, only: matmul_t
+   use matmul_m, only: matmul_t
 
-    instantiate matmul_t(real, real, real, operator(*), sum), only: matmul
+   call simple_driver()
+   call block_matmul_driver()
+   
+contains
 
-    real :: x(3, 2), y(2, 3), z(3, 3)
 
-    x = reshape([(real(i), i = 1, 6)], [3, 2])
-    y = reshape([(real(i), i = 6, 1, -1)], [2, 3])
-    z = matmul(x, y)
-    do i = 1, 3
-      print *, z(i,:)
-    end do
+   ! The following procedure implements and uses matrix multiplication
+   ! of an ordinary real array and an ordinary integer array to produce
+   ! an ordinary real array.
+   subroutine simple_driver()
+
+      INSTANTIATE matmul_t(real, integer, real, times, sum), only: matmul
+
+      real :: x(3, 2), y(2, 3), z(3, 3)
+
+      x = reshape([(real(i), i = 1, 6)], [3, 2])
+      y = reshape([(real(i), i = 6, 1, -1)], [2, 3])
+
+      z = matmul(x, y)
+
+      do i = 1, 3
+         print *, z(i,:)
+      end do
+
+   contains
+
+      elemental function times(x,y) result(z)
+         real :: z
+         real, intent(in) :: x
+         integer, intent(in) :: i
+
+         z = x*i
+      end function times
+      
+   end subroutine simple_driver
+
+
+   subroutine block_matmul_driver()
+
+      type :: RealBlock
+         real, allocatable :: elements(:,:)
+      end type RealBlock
+
+      type :: ComplexBlock_block
+         complex, allocatable :: elements(:,:)
+      end type ComplexBlock_block
+
+      INSTANTIATE matmul_t(RealBlock, ComplexBlock, ComplexBlock, times, sum), only: block_matmul => matmul
+
+      type(RealBlock)    :: A(7,2)
+      type(ComplexBlock) :: B(2,3)
+      type(ComplexBlock) :: C(7,3)
+
+      call initialize_A(A)
+      call initialize_B(B)
+
+      C = block_matmul(A, B)
+
+      call  print_results(C)
+
+   contains
+
+      pure function sum(x) result(s)
+         type(ComplexBlock) :: s
+         type(ComplexBlock), intent(in) :: x(:)
+
+         integer :: i
+         
+         s%elements = 0
+         do i = 1:size(x)
+            s%elements = s%elements + x%elements
+         end do
+
+      end function sum
+
+      elemental function times(x,y) result(z)
+         type(ComplexBlock) :: z
+         type(RealBlock), intent(in) :: x
+         type(ComplexBlock), intent(in) :: y
+         real, intent(in) :: x
+         integer, intent(in) :: i
+
+         ! Exploit optimized built-in matmul
+         z%elements = matmul(x%elements, y%elements)
+      end function times
+      
+   end subroutine simple_driver
+
+
+
 end program
--- examples/matmul/matmul_m.f90
+++ examples/matmul/matmul_m.f90
@@ -3,44 +3,64 @@ module matmul_m
     private
     public :: matmul_t
 
-    restriction matmulable(T, U, V, times, sum)
-        type :: T; end type
-        type :: U; end type
-        type :: V; end type
-        interface
-            elemental function times(x, y) result(prod)
-                type(T), intent(in) :: x
-                type(U), intent(in) :: y
-                type(V) :: prod
-            end function
+    ! sum(T) --> T
+    RESTRICTION reduction_operation(T, op)
+       type :: T; end type
+       interface
+          pure function op(arr)
+             type(T) :: arr(:)
+             type(T) :: op
+          end function op
+       end interface
+    END RESTRICTION
 
-            function sum(arr)
-                type(V) :: arr(:)
-                type(V) :: sum
-            end function
-        end interface
-    end restrction
+    ! T * U -> V
+    RESTRICTION binary_operation(op, T, U, V)
+       type :: T; end types
+       type :: U; end type
+       type :: V; end type
+       interface
+           elemental function op(x, y) result(z)
+              type(T), intent(in) :: x
+              type(U), intent(in) :: y
+              type(V) :: z
+           end function
+       end interface
+    END RESTRICTION
 
-    template matmul_t(T, U, V, times, sum)
+    !---------------------------
+    TEMPLATE matmul_t(T, U, V, times, sum)
         private
         public :: matmul
-        requires matmulable(T, U, V, times, sum)
+        REQUIRES binary_operation(times, T, U, V)
+        REQUIRES reduction_operation(sum, V)
+
         interface matmul
             module procedure matmul_
         end interface
+
     contains
+
         function matmul_(A, B) result(C)
             type(T), intent(in) :: A(:,:)
             type(U), intent(in) :: B(:,:)
             type(V), allocatable :: C(:, :)
 
             integer :: i, j
+            integer :: ni, nj, nk
 
-            if (size(A, dim=2) /= size(B, dim=1)) error stop "Mismatched matrix sizes"
-            allocate(C(size(A, dim=1), size(B, dim=2)))
-            do concurrent(i = 1 : size(A, dim=1), j = 1 : size(B, dim=2))
+            ni = size(A,dim=1)
+            nj = size(B,dim=2)
+            nk = size(A,dim=2)
+
+            if (size(B, dim=1) /= nk) error stop "Mismatched matrix sizes"
+
+            allocate(C(ni,nj))
+            do concurrent (i = 1:ni, j = 1:nj)
                 C(i,j) = sum(times(A(i,:),B(:,j)))
             end do
         end function
-    end template
+
+    END TEMPLATE
+    !---------------------------
 end module
