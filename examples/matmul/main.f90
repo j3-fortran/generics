b2208e9c11c9bc7e5ec7db359470d8a086320177
Tom Clune <thomas.l.clune@nasa.gov>
2022-07-11 15:03:49 -0400

    Extended example further:
    - Introduced MatrixBlock_t template for nesting matrices
    - Introduced inner template reduce() that acts on MatrixBlock
    - Renamed module to LinearAlgebra_m

--- examples/matmul/main.f90
+++ examples/matmul/main.f90
@@ -1,8 +1,19 @@
 program main
-   use matmul_m, only: matmul_t
+   use LinearAlgebra_m, only: MatrixMultiply_t, MatrixBlock_t
+
+
+   instantiate MatrixBlock_t(real), only: RealBlock => MatrixBlock, reduce_RealBlock_t => reduce_t
+   instantiate reduce_RealBlock_t(sum), only: sum => reduce
+
+   instantiate MatrixBlock_t(complex), only: ComplexBlock => MatrixBlock, reduce_ComplexBlock_t => reduce_t
+   instantiate reduce_ComplexBlock_t(sum), only: sum => reduce
+
+   instantiate MatrixBlock_t(RealBlock), only: NestedRealBlock => MatrixBlock
+   instantiate MatrixBlock_t(ComplexBlock), only: NestedComplexBlock => MatrixBlock
 
    call simple_driver()
    call block_matmul_driver()
+   call nested_driver()
    
 contains
 
@@ -12,7 +23,7 @@ contains
    ! an ordinary real array.
    subroutine simple_driver()
 
-      INSTANTIATE matmul_t(real, integer, real, times, sum), only: matmul
+      INSTANTIATE MatrixMultiply_t(real, integer, real, times, sum), only: matmul
 
       real :: x(3, 2), y(2, 3), z(3, 3)
 
@@ -40,15 +51,7 @@ contains
 
    subroutine block_matmul_driver()
 
-      type :: RealBlock
-         real, allocatable :: elements(:,:)
-      end type RealBlock
-
-      type :: ComplexBlock_block
-         complex, allocatable :: elements(:,:)
-      end type ComplexBlock_block
-
-      INSTANTIATE matmul_t(RealBlock, ComplexBlock, ComplexBlock, times, sum), only: block_matmul => matmul
+      INSTANTIATE MatrixMultiply_t(RealBlock, ComplexBlock, ComplexBlock, matmul, sum), only: block_matmul => matmul
 
       type(RealBlock)    :: A(7,2)
       type(ComplexBlock) :: B(2,3)
@@ -61,33 +64,26 @@ contains
 
       call  print_results(C)
 
-   contains
+   end subroutine block_matmul_driver
 
-      pure function sum(x) result(s)
-         type(ComplexBlock) :: s
-         type(ComplexBlock), intent(in) :: x(:)
 
-         integer :: i
-         
-         s%elements = 0
-         do i = 1:size(x)
-            s%elements = s%elements + x%elements
-         end do
+   subroutine nested_driver()
 
-      end function sum
+      INSTANTIATE MatrixMultiply_t(RealBlock, ComplexBlock, ComplexBlock, times, sum), only: block_matmul => matmul
+      INSTANTIATE MatrixMultiply_t(NestedRealBlock, NestedComplexBlock, NestedComplexBlock, block_matmul, sum), only: nested_matmul => matmul
 
-      elemental function times(x,y) result(z)
-         type(ComplexBlock) :: z
-         type(RealBlock), intent(in) :: x
-         type(ComplexBlock), intent(in) :: y
-         real, intent(in) :: x
-         integer, intent(in) :: i
+      type(NestedRealBlock)    :: A(7,2)
+      type(NestedComplexBlock) :: B(2,3)
+      type(NestedComplexBlock) :: C(7,3)
 
-         ! Exploit optimized built-in matmul
-         z%elements = matmul(x%elements, y%elements)
-      end function times
-      
-   end subroutine simple_driver
+      call initialize_A(A)
+      call initialize_B(B)
+
+      C = nested_matmul(A, B)
+
+      call  print_results(C)
+
+   end subroutine nested_driver
 
 
 
--- examples/matmul/matmul_m.f90
+++ examples/matmul/matmul_m.f90
@@ -1,7 +1,9 @@
-module matmul_m
+module LinearAlgebra_m
     implicit none
     private
-    public :: matmul_t
+
+    public :: MatrixMultiply_t
+    public :: MatrixBlock_t
 
     ! sum(T) --> T
     RESTRICTION reduction_operation(T, op)
@@ -28,8 +30,9 @@ module matmul_m
        end interface
     END RESTRICTION
 
+
     !---------------------------
-    TEMPLATE matmul_t(T, U, V, times, sum)
+    TEMPLATE MatrixMultiply_t(T, U, V, times, sum)
         private
         public :: matmul
         REQUIRES binary_operation(times, T, U, V)
@@ -63,4 +66,48 @@ module matmul_m
 
     END TEMPLATE
     !---------------------------
+
+
+    TEMPLATE MatrixBlock_t(T)
+       public :: MatrixBlock
+       public :: Reduce_T
+
+       type :: T; end type
+
+       type MatrixBlock
+          type(T), allocatable :: elements(:,:)
+       end type MatrixBlock
+
+       TEMPLATE Reduce_T(op)
+          REQUIRES reduction_operation(T, op)
+
+          interface reduce
+             module procedure :: reduce_
+          end interface reduce
+
+       contains
+
+          pure function reduce_(x) result(y)
+             type(T), intent(in) :: x(:)
+             type(T) :: y
+
+             integer :: i, j
+             integer :: ni, nj
+
+             if (size(x) == 0) error stop 'empty array unsupported'
+
+             ni = size(x(1)%elements,dim=1)
+             nj = size(x(1)%elements,dim=2)
+             allocate(y%elements(ni, nj))
+             
+             do concurrent (i = 1:ni, j = 1:nj)
+                y%elements(i,j) = op(x(:)%elements(i,j))
+             end do
+
+          end function reduce_
+
+       end function sum
+
+    END TEMPLATE
+
 end module
